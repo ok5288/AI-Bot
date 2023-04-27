d3f16098bf564adb1b785242765949cad6a42997
ok5266 <52518135+ok5266@users.noreply.github.com>
2023-04-28 00:03:33 +0800

    Update GithubSite.vue

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/ok5266/AI-Bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>Homepage: </span>
+    <a href="http://www.ok5266.com" target="_blank" class="text-blue-500">
+      OK5266
     </a>
   </div>
 </template>
